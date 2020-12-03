fb5948dc022da0d932726e4f385ac087defa0be9
aden <adenrashid@hotmail.com>
2020-12-03 11:36:59 +1100

    refactoring, fixed some warnings, fixed filter buttons

--- src/Incidents.js
+++ src/Incidents.js
@@ -1,62 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Incidents( {markers} ) {
 
-    const [timeDescending, setTimeDescending] = React.useState(false);
-    const [timeAscending, setTimeAscending] = React.useState(false);
+  const [timeDescending, setTimeDescending] = useState(false);
+  const [timeAscending, setTimeAscending] = useState(false);
   
-    const sortTimeDescending = 
-        markers.map((marker, index) => {
-            return ( 
-            <div className="incident-reports-wrap">
-                <li className="incident-reports" key={marker[index]}>
-                <p>Address: {marker.location}</p>
-                <p>Pinned Location: {marker.lat}, {marker.lng}</p>
-                <p>Time Reported: {marker.time.toString()}</p>
-                <p>Latest Update: {marker.description}</p>
-                </li>
-            </div>
-            )
-        })
-  
-    const markersReversed = [...markers].reverse();
-  
-    const sortTimeAscending = 
-      markersReversed.map((marker, index) => {
+  const sortTimeDescending = 
+    markers.map((marker, index) => {
         return ( 
-          <div className="incident-reports-wrap">
-            <li className="incident-reports" key={marker[index]}>
-              <p>Address: {marker.location}</p>
-              <p>Pinned Location: {marker.lat}, {marker.lng}</p>
-              <p>Time Reported: {marker.time.toString()}</p>
-              <p>Latest Update: {marker.description}</p>
+        <div className="incident-reports-wrap" key={index}>
+            <li className="incident-reports">
+            <p>Address: {marker.location}</p>
+            <p>Pinned Location: {marker.lat}, {marker.lng}</p>
+            <p>Time Reported: {marker.time.toString()}</p>
+            <p>Latest Update: {marker.description}</p>
             </li>
-          </div>
+        </div>
         )
-      })
+    })
   
-    return (
-      <div>
-        <h1 className="title">ReportIt</h1>
-          <h2 className="subtitle">Pin an incident near you</h2>
-          <h2 className="view-incidents">Reported Incidents</h2>
-          <h2 className="filter">Filter By:</h2>
-          <div className="filters-wrap">
-            <button className="filters" onClick={
-              () => setTimeDescending(true)
-              }>Time Reported (Descending)
-            </button>
-            <button className="filters" onClick={
-              () => setTimeAscending(true)
-              }>Time Reported (Ascending)
-            </button>
-          </div>
-          <ul className="incident-ul">
-            {timeDescending ? sortTimeDescending : ''}
-            {timeAscending ? sortTimeAscending : ''}
-          </ul>
-      </div>
-    )
+  const markersReversed = [...markers].reverse();
+
+  const sortTimeAscending = 
+    markersReversed.map((marker, index) => {
+      return ( 
+        <div className="incident-reports-wrap" key={index}>
+          <li className="incident-reports">
+            <p>Address: {marker.location}</p>
+            <p>Pinned Location: {marker.lat}, {marker.lng}</p>
+            <p>Time Reported: {marker.time.toString()}</p>
+            <p>Latest Update: {marker.description}</p>
+          </li>
+        </div>
+      )
+    })
+
+  const descending = () => {
+    setTimeDescending(true)
+    setTimeAscending(false)
+  }
+
+  const ascending = () => {
+    setTimeAscending(true)
+    setTimeDescending(false)      
+  }
+  
+  return (
+    <div>
+      <h1 className="title">ReportIt</h1>
+        <h2 className="subtitle">Pin an incident near you</h2>
+        <h2 className="view-incidents">Reported Incidents</h2>
+        <h2 className="filter">Filter By:</h2>
+        <div className="filters-wrap">
+          <button className="filters" onClick={descending}>Time Reported (Descending)
+          </button>
+          <button className="filters" onClick={ascending}>Time Reported (Ascending)
+          </button>
+          {/* <button className="filters" onClick={
+            () => setTimeAscending(true)
+            }>Location (Closest to me)
+          </button> */}
+          {/* <UserCurrentLocation markers={markers}/> */}
+        </div>
+        <ul className="incident-ul">
+          {timeDescending ? sortTimeDescending : ''}
+          {timeAscending ? sortTimeAscending : ''}
+        </ul>
+    </div>
+  )
+
 };
+
+// function UserCurrentLocation( {markers} ) {
+
+//     const sortUserCoords = ({lat, lng}) => {
+//         console.log(
+//             Math.sqrt(Math.abs(lat)) + Math.sqrt(Math.abs(lng))
+//         )
+//     };
+
+//     const sortMarkerCoords = (markers) => {
+//         markers.map(marker => {
+//             return console.log(
+//                 Math.sqrt(Math.abs(marker.lat)) + Math.sqrt(Math.abs(marker.lng))
+//             )
+//         })
+//     }
+
+//     return ( 
+      // <button className="locate" onClick={() => {
+      //   // navigator.geolocation.getCurrentPosition((position) => {
+      //   //         {
+      //   //     lat: position.coords.latitude,
+      //   //     lng: position.coords.longitude
+      //   //   }
+      //       sortMarkerCoords(markers)
+      //   //   sortUserCoords()
+      //   // }, () => null)
+      // }}>
+//         <img src="compass.png" alt="compass - pin to locate me" />
+//         <p className="description">Find my current location</p>
+//       </button>
+//     );
+// };
 
 export default Incidents
--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState, useRef } from 'react';
 import { 
     GoogleMap, 
     useLoadScript,
@@ -38,219 +38,219 @@ const options = {
 
 function Map({ markers, setMarkers }) {
 
-    const { isLoaded, loadError } = useLoadScript({
-      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-      libraries,
-    });
-    const [selected, setSelected] = React.useState(null);
-    const [loading, setLoading] = React.useState(false);
-    const [input, setInput] = React.useState('');
-  
-    const onMapClick = React.useCallback((e) => {
-      Geocode.fromLatLng(e.latLng.lat(), e.latLng.lng()).then(
-        response => {
-          const address = response.results[0].formatted_address;
-          setMarkers((current) => [
-            ...current,
-            {
-              lat: e.latLng.lat(),
-              lng: e.latLng.lng(),
-              time: new Date(),
-              description: "",
-              location: address
-            }
-          ]);
-        },
-        error => {
-          console.error(error);
-        }
-      ) 
-    }, []);
-  
-    const onMapDrag = React.useCallback((e, index) => {
-      Geocode.fromLatLng(e.latLng.lat(), e.latLng.lng()).then(
-        response => {
-          const address = response.results[0].formatted_address;
-          setMarkers((current) => {
-            const markers = [...current];
-            markers[index] = { 
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const [selected, setSelected] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('');
+
+  const onMapClick = useCallback((e) => {
+    Geocode.fromLatLng(e.latLng.lat(), e.latLng.lng()).then(
+      response => {
+        const address = response.results[0].formatted_address;
+        setMarkers((current) => [
+          ...current,
+          {
             lat: e.latLng.lat(),
             lng: e.latLng.lng(),
             time: new Date(),
-            description: markers[index].description, 
+            description: "",
             location: address
-            }
-            return markers;
-          });
-        },
-        error => {
-          console.error(error);
-        }
-      ) 
-    }, []);
-  
-    const updateDescription = React.useCallback((e) => {
-      setInput(e.target.value);
-      if (e.target.value.length > 0) {
-        setLoading(true)
-      } else {
-        setLoading(false)
+          }
+        ]);
+      },
+      error => {
+        console.error(error);
       }
-    })
+    ) 
+  }, [setMarkers]);
   
-    const onSubmit = React.useCallback((e) => {
-      e.preventDefault();
-      markers[markers.indexOf(selected)].description = `${input}`;
-      setLoading(false);
-      setInput('');
-    })
+  const onMapDrag = useCallback((e, index) => {
+    Geocode.fromLatLng(e.latLng.lat(), e.latLng.lng()).then(
+      response => {
+        const address = response.results[0].formatted_address;
+        setMarkers((current) => {
+          const markers = [...current];
+          markers[index] = { 
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng(),
+          time: new Date(),
+          description: markers[index].description, 
+          location: address
+          }
+          return markers;
+        });
+      },
+      error => {
+        console.error(error);
+      }
+    ) 
+  }, [setMarkers]);
+
+  const updateDescription = useCallback((e) => {
+    setInput(e.target.value);
+    if (e.target.value.length > 0) {
+      setLoading(true)
+    } else {
+      setLoading(false)
+    }
+  }, []);
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    markers[markers.indexOf(selected)].description = `${input}`;
+    setLoading(false);
+    setInput('');
+  }, [input, markers, selected]);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = useCallback(({lat, lng}) => {
+    mapRef.current.panTo({lat, lng});
+    mapRef.current.setZoom(14);
+  }, []);
+    
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div>
+      <h1 className="title">ReportIt</h1>
+      <h2 className="subtitle">Pin an incident near you</h2>
+
+      <Search panTo={panTo}/>
+      <Locate panTo={panTo}/>
+
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle} 
+        zoom={12} 
+        center={center} 
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker, index) => (
+          <Marker 
+            key={index}
+            time={marker.time.toISOString()}
+            position={{lat: marker.lat, lng: marker.lng}}
+            draggable={true}
+            icon={{
+              url: '/car.png',
+              scaledSize: new window.google.maps.Size(30,30),
+              origin: new window.google.maps.Point(0,0),
+              anchor: new window.google.maps.Point(15,15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            onDragEnd={
+              (e) => onMapDrag(e, index)
+            }
+          />
+        ))}
   
-    const mapRef = React.useRef();
-    const onMapLoad = React.useCallback((map) => {
-      mapRef.current = map;
-    }, []);
-  
-    const panTo = React.useCallback(({lat, lng}) => {
-      mapRef.current.panTo({lat, lng});
-      mapRef.current.setZoom(14);
-    }, []);
-      
-    if (loadError) return "Error loading maps";
-    if (!isLoaded) return "Loading Maps";
-  
-    return (
-      <div>
-        <h1 className="title">ReportIt</h1>
-        <h2 className="subtitle">Pin an incident near you</h2>
-  
-        <Search panTo={panTo}/>
-        <Locate panTo={panTo}/>
-  
-        <GoogleMap 
-          mapContainerStyle={mapContainerStyle} 
-          zoom={12} 
-          center={center} 
-          options={options}
-          onClick={onMapClick}
-          onLoad={onMapLoad}
-        >
-          {markers.map((marker, index) => (
-            <Marker 
-              key={index}
-              time={marker.time.toISOString()}
-              position={{lat: marker.lat, lng: marker.lng}}
-              draggable={true}
-              icon={{
-                url: '/car.png',
-                scaledSize: new window.google.maps.Size(30,30),
-                origin: new window.google.maps.Point(0,0),
-                anchor: new window.google.maps.Point(15,15),
-              }}
-              onClick={() => {
-                setSelected(marker);
-              }}
-              onDragEnd={
-                (e) => onMapDrag(e, index)
-              }
-            />
-          ))}
-   
-          {selected ? (
-            <InfoWindow
-            position={{lat: selected.lat, lng: selected.lng}} 
-            onCloseClick={() => {setSelected(null)}}
-            >
-              <div>
-                <h2>Incident Reported!</h2>
-                <form 
-                  action=""
-                  onSubmit={onSubmit}>
-                    <label htmlFor="">Add an update:</label>
-                    <input
-                      className="update-input"
-                      value={input} 
-                      onChange={
-                        (e) => updateDescription(e)
-                      }
-                    />
-                  <button>Update</button>
-                  {loading ? <span className="loading">🔷</span> : ''}
-                </form>
-                <p>Latest Update: {selected.description}</p>  
-                <p>Location: {selected.location} </p>
-                <p>Reported: { formatRelative(selected.time, new Date()) }</p>
-              </div>
-            </InfoWindow>
-          ) : null}
-        </GoogleMap>
-      </div>
-    )
+        {selected ? (
+          <InfoWindow
+          position={{lat: selected.lat, lng: selected.lng}} 
+          onCloseClick={() => {setSelected(null)}}
+          >
+            <div>
+              <h2>Incident Reported!</h2>
+              <form 
+                action=""
+                onSubmit={onSubmit}>
+                  <label htmlFor="">Add an update:</label>
+                  <input
+                    className="update-input"
+                    value={input} 
+                    onChange={
+                      (e) => updateDescription(e)
+                    }
+                  />
+                <button>Update</button>
+                {loading ? <span className="loading">🔷</span> : ''}
+              </form>
+              <p>Latest Update: {selected.description}</p>  
+              <p>Location: {selected.location} </p>
+              <p>Reported: { formatRelative(selected.time, new Date()) }</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
+  )
 };
 
 function Search({panTo}) {
-    const {
-      ready, 
-      value, 
-      suggestions: {status, data}, 
-      setValue, 
-      clearSuggestions
-    } = usePlacesAutocomplete({
-      requestOptions: {
-        location: {lat: () => -37.700560, lng: () => 144.736810 },
-        radius: 20 * 1000,
+  const {
+    ready, 
+    value, 
+    suggestions: {status, data}, 
+    setValue, 
+    clearSuggestions
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: {lat: () => -37.700560, lng: () => 144.736810 },
+      radius: 20 * 1000,
+    }
+  })
+
+  return (
+    <Combobox onSelect={async (address) => {
+
+      setValue(address, false);
+      clearSuggestions();
+
+      try {
+        const results = await getGeocode({address});
+        const { lat, lng } = await getLatLng(results[0]);
+        panTo({ lat, lng });
+      } catch(error) {
+      console.log("error!")
       }
-    })
+    }}>
   
-    return (
-      <Combobox onSelect={async (address) => {
+    <ComboboxInput 
+      value={value} 
+      onChange={(e) => {
+        setValue(e.target.value)
+      }} 
+      disabled={!ready}
+      placeholder="Enter a location"
+      className="search"
+    />
   
-        setValue(address, false);
-        clearSuggestions();
-  
-        try {
-          const results = await getGeocode({address});
-          const { lat, lng } = await getLatLng(results[0]);
-          panTo({ lat, lng });
-        } catch(error) {
-        console.log("error!")
-        }
-      }}>
-    
-      <ComboboxInput 
-        value={value} 
-        onChange={(e) => {
-          setValue(e.target.value)
-        }} 
-        disabled={!ready}
-        placeholder="Enter a location"
-        className="search"
-      />
-    
-      <ComboboxPopover>
-        <ComboboxList>
-        {status === "OK" && 
-        data.map(({id, description}) => 
-          <ComboboxOption key={id} value={description} />)}
-          </ComboboxList>
-          </ComboboxPopover>
-      </Combobox>
-    ); 
+    <ComboboxPopover>
+      <ComboboxList>
+      {status === "OK" && 
+      data.map(({id, description}) => 
+        <ComboboxOption key={id} value={description} />)}
+        </ComboboxList>
+        </ComboboxPopover>
+    </Combobox>
+  ); 
 };
 
 function Locate({panTo}) {
-    return ( 
-      <button className="locate" onClick={() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          panTo({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          })
-        }, () => null)
-      }}>
-        <img src="compass.png" alt="compass - pin to locate me" />
-        <p className="description">Find my current location</p>
-      </button>
-    );
+  return ( 
+    <button className="locate" onClick={() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        panTo({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        })
+      }, () => null)
+    }}>
+      <img src="compass.png" alt="compass - pin to locate me" />
+      <p className="description">Find my current location</p>
+    </button>
+  );
 };
 
 export default Map
